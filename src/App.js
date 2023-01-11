c92abcc9290e1e0d76227d0d6295f8cf3dab9194
Suzuki Sachiko <suzuki.sa.www@gmail.com>
2023-01-11 17:11:07 +0900

    バックエンドと接続

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react'
+
 
 function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
+      <p>{ message }</p>
     </div>
   );
 }
